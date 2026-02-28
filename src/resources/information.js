0cfb272a26f6130080307a7e94d0e01df5c5b06e
tammyding-sys <tammyding@gmail.com>
2026-02-27 17:16:24 -0700

    Update information.js
    Edited information

--- src/resources/information.js
+++ src/resources/information.js
@@ -1,6 +1,6 @@
-export const ADDRESS_LINE_1 = '123 Address St';
+export const ADDRESS_LINE_1 = '14640 6 St SW, Calgary, AB';
 export const ADDRESS_LINE_2 = 'Calgary, AB';
-export const ADDRESS_LINE_3 = 'A1A 1A1';
+export const ADDRESS_LINE_3 = 'T2Y 0E1';
 
-export const PHONE_NUMBER = '123-456-7890';
-export const EMAIL_ADDRESS = 'email@email.com';
+export const PHONE_NUMBER = '587-889-7571';
+export const EMAIL_ADDRESS = 'cgtc201204@gmail.com';
